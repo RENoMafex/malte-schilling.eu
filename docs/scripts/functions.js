fd50b430d2238cfb40aa38bdd5baf17147360219
Malte Schilling <git@malte-schilling.eu>
2026-05-09 17:01:54 +0200

    switch from js to ts

--- docs/scripts/functions.js
+++ docs/scripts/functions.js
@@ -1,15 +1,14 @@
 //@ts-check
-
 function openNav() {
-	const nav = document.getElementById('navbar');
-
-	if (nav == null) return;
-
-	window.scrollTo(0,0);
-
-	if (!nav.classList.contains('responsive')) {
-		nav.classList.add('responsive');
-	}else{
-		nav.classList.remove('responsive');
-	}
+    const nav = document.getElementById('navbar');
+    if (nav == null)
+        return;
+    window.scrollTo(0, 0);
+    if (!nav.classList.contains('responsive')) {
+        nav.classList.add('responsive');
+    }
+    else {
+        nav.classList.remove('responsive');
+    }
 }
+export {};
--- docs/scripts/on-ready.js
+++ docs/scripts/on-ready.js
@@ -1,40 +1,39 @@
 //@ts-check
-
 document.addEventListener('DOMContentLoaded', () => {
-	const links = document.querySelectorAll('#navbar a');
-	const currentPath = window.location.pathname;
-	const nav = document.getElementById('navbar');
-
-
-	links.forEach(link => {
-		const linkPath = new URL(link.href).pathname;
-		if (currentPath === linkPath || currentPath.endsWith(linkPath)) {
-			link.classList.add('active');
-		}
-	});
-
-	document.querySelectorAll('.dropdown').forEach(dropdown => {
-		const isActive = dropdown.querySelector('.dropdown-content a.active');
-		if (isActive) {
-			const dropbutton = dropdown.querySelector('.dropbutton');
-			if (dropbutton) {
-				dropbutton.classList.add('active');
-			}
-		}
-	});
-
-	document.querySelectorAll('.dropbutton').forEach(button => {
-		button.addEventListener('click', (event) => {
-			if (nav == null) return;
-			if (!nav.classList.contains('responsive')) {
-				return;
-			}
-			event.preventDefault();
-			const dropdown = button.closest('.dropdown');
-			if (!dropdown) return;
-			const content = dropdown.querySelector('.dropdown-content');
-			if (!content) return;
-			content.classList.toggle('show');
-		});
-	});
+    const links = document.querySelectorAll('#navbar a');
+    const currentPath = window.location.pathname;
+    const nav = document.getElementById('navbar');
+    links.forEach(link => {
+        const linkPath = new URL(link.href).pathname;
+        if (currentPath === linkPath || currentPath.endsWith(linkPath)) {
+            link.classList.add('active');
+        }
+    });
+    document.querySelectorAll('.dropdown').forEach(dropdown => {
+        const isActive = dropdown.querySelector('.dropdown-content a.active');
+        if (isActive) {
+            const dropbutton = dropdown.querySelector('.dropbutton');
+            if (dropbutton) {
+                dropbutton.classList.add('active');
+            }
+        }
+    });
+    document.querySelectorAll('.dropbutton').forEach(button => {
+        button.addEventListener('click', (event) => {
+            if (nav == null)
+                return;
+            if (!nav.classList.contains('responsive')) {
+                return;
+            }
+            event.preventDefault();
+            const dropdown = button.closest('.dropdown');
+            if (!dropdown)
+                return;
+            const content = dropdown.querySelector('.dropdown-content');
+            if (!content)
+                return;
+            content.classList.toggle('show');
+        });
+    });
 });
+export {};
