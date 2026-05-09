59b9b9dbbf2ddcb3db2fc43dd5fcd90b27714bc0
Malte Schilling <git@malte-schilling.eu>
2026-05-09 16:13:42 +0200

    began moving scripts out of HTML

---
+++ docs/scripts/functions.js
@@ -0,0 +1,15 @@
+//@ts-check
+
+function openNav() {
+	const nav = document.getElementById('navbar');
+
+	if (nav == null) return;
+
+	window.scrollTo(0,0);
+
+	if (!nav.classList.contains('responsive')) {
+		nav.classList.add('responsive');
+	}else{
+		nav.classList.remove('responsive');
+	}
+}
